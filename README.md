6e216c14caf34007afd57506dffbd51991122ef4
Samuel Cristobal <sc@databeacon.aero>
2021-10-05 21:56:56 +0200

    setup and minimal example

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # clean-this-mess
-A simple utility to subscribe and run tasks before closing a nodejs program 
+
+A simple utility to subscribe and run tasks before closing a nodejs program
+
+## Usage
+
+Import the `cleanup` factory as a named ESM export. Upon calling it, `cleanup` creates a subscription. Pass any function (even asynchronous) that you want to run before the program exits.
+
+```typescript
+import { cleanup } from './src/index.ts';
+
+const goodbye = cleanup();
+
+goodbye(() => 'Goodbye 👋');
+```
+
+Note: order of execution is not warrantied, if your functions are async they do not wait for each other, they are added to the micro task queue at once, eg. `Promise.all`
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { log } from 'console';
+import chalk from 'chalk';
+
+const { yellowBright, redBright, red } = chalk;
+
+type Task = () => void | Promise<void>;
+
+const cleanup = () => {
+    const tasks: Task[] = [];
+
+    process.on('exit', async () => {
+        log(yellowBright(`\nRunning ${tasks.length} scheduled tasks... : 🧹 `));
+
+        await Promise.all(tasks.map(async (func) => await func()));
+
+        process.exit();
+    });
+
+    process.on('SIGINT', () => {
+        log(redBright(`\nGracefully shutting down from SIGINT (Ctrl-C)`));
+        process.exit(2);
+    } );
+
+
+     process.on('uncaughtException',  (error) {
+         console.log(red(`\nExiting not so gracefully due to an uncaught Exception...`));
+         console.log(error.stack);
+         process.exit(99);
+     });
+
+    return function schedule(task: Task) {
+        tasks.push(task);
+    };
+};
+
+export { cleanup };
+
+    
+    
